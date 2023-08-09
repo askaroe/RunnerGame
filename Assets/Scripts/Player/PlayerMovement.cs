41978302e2ed24f09fb715d7a13f81e907b1db83
Askar Oralkhan <askar4ik123@gmail.com>
2023-08-09 11:07:06 +0600

    feat: updated player movement, player can move only between three lines

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,23 +5,31 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private float speed;
     [SerializeField] private float horizontalMultiplier = 2.0f;
     private Rigidbody _rb;
-    private float _horizontalInput;
+    private float _lineDifference = 2.2f;
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody>();
     }
 
-    private void FixedUpdate()
-    {
-        Vector3 forwardMove = transform.forward * (speed * Time.fixedDeltaTime);
-        Vector3 horizontalMove = transform.right * (_horizontalInput * speed * Time.fixedDeltaTime * horizontalMultiplier);
-        _rb.MovePosition(_rb.position + forwardMove + horizontalMove);
-    }
 
     private void Update()
     {
-        _horizontalInput = Input.GetAxisRaw("Horizontal");
+        Movement();
+    }
+
+    private void Movement()
+    {
+        Vector3 forwardMove = transform.forward * (speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            _rb.MovePosition(transform.position + new Vector3(-_lineDifference, 0, 0));
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            _rb.MovePosition(transform.position + new Vector3(_lineDifference, 0, 0));
+        }
+        _rb.MovePosition(_rb.position + forwardMove);
     }
 
     public void StartRunning()
